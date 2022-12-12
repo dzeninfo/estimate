75eaa185349d192022b3700b3dd2a49bf243e83e
Dmitri <dzeninfo@gmail.com>
2022-12-12 16:42:48 +0200

    input field in expense

--- src/components/Projects/Expenses/Expense.js
+++ src/components/Projects/Expenses/Expense.js
@@ -1,11 +1,22 @@
-import styles from './Expense.module.css';
 import { MdArrowUpward, MdArrowDownward } from 'react-icons/md';
+import { useState } from 'react';
 import DeleteButton from './../../UI/DeleteButton';
-
+import styles from './Expense.module.css';
 
 function Expense(props) {
   const { index, chapterIndex, deleteExpenses, expense } = props;
-  console.log(expense);
+
+  const [quntity, setQuantity] = useState(0);
+  const [costPerUnit, setCostPerUnit] = useState(0);
+  const [total, setTotal] = useState(0);
+  // const total = group.reduce((acc, cur) => {
+  //   acc += cur.g1 + cur.g2 + cur.g3;
+  //   return acc;
+  // }, 0);
+  const calculate = () => {
+    setTotal(quntity*costPerUnit);
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.row}>
@@ -28,18 +39,36 @@ function Expense(props) {
           </div>
         </div>
         <div className={styles.column}>
-          <div className={styles.item}>1</div>
+          <div className={styles.item}>AliExpress</div>
         </div>
         <div className={styles.column}>
-          <div className={styles.item}>1</div>
+          <div className={styles.item}>
+            <input
+              className={styles.quantityInput}
+              type="number"
+              value={quntity}
+              onChange={(e) => setQuantity(+e.target.value)}
+              placeholder="0"
+            />
+          </div>
         </div>
         <div className={styles.column}>
-          <div className={styles.item}>1</div>
+          <div className={styles.item}>
+            <input className={styles.unitInput} placeholder="m2" />
+          </div>
         </div>
         <div className={styles.column}>
           <div className={styles.row}>
-            <div className={styles.item}>10</div>
-            <div className={styles.item}>2500</div>
+            <div className={styles.item}>
+              <input
+                className={styles.perUnitInput}
+                type="number"
+                value={costPerUnit}
+                onChange={(e) => setCostPerUnit(+e.target.value)}
+                placeholder="0"
+              />
+            </div>
+            <div className={styles.item}>{total}</div>
           </div>
         </div>
       </div>
